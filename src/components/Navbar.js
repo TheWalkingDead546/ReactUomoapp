02879a2e4ef2271c9d720a51adb4c4ddbe30d67e
TheWalingDead546 <rouhankhatri2017@gmail.com>
2024-09-02 23:56:21 +0500

    initialfiles

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import { click } from '@testing-library/user-event/dist/click';
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    const [click,setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+    const closeMenu = () => setClick(false)
+  return (
+    <>
+    <nav className='navbar'>
+        <div className='navbar-container'>
+            <Link to="/" className="navbar-logo">
+                UOM <i class="fa-solid fa-circle"></i>
+            </Link>
+            <div className='menu-icon' onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-links' onClick={closeMenu}>
+                        Women
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-links' onClick={closeMenu}>
+                        Men
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-links' onClick={closeMenu}>
+                        Kids
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar;
