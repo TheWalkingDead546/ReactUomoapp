4f97d8bd3d9368b98a7f87c924f176a27da39003
Ziyadk147 <Ziyadkhan2333@gmail.com>
2024-09-03 23:30:33 +0500

    split the nav bar component into Navlist and Navsublist
    made the functionality to change the content of the sublist based on the buttons in the list

---
+++ src/components/Navbar/Navsublist/Navsublist.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default function Navsublist({selectedButton , isActive}) {
+    let menuList = [];
+    if (selectedButton === "women"){
+       menuList =   [
+            "Accessories",
+            "Women",
+            "Kids",
+            "Home",
+            "Collection",
+            "Sale up to 50% Off",
+            "Jeans",
+            "Dresses",
+            "Shoes",
+            "Join Life"
+        ]
+    }
+    else if (selectedButton === "men"){
+        menuList =   [
+            "Accessories",
+            "Join Life",
+            "T-Shirts & Tops",
+            "Gift Card",
+            "Jackets & Coats",
+            "Best Sellers",
+            "Collection",
+            "Sale up to 50% Off",
+            "#UomoStyle"
+        ]
+    }
+    else{
+        menuList =   [
+            "Man",
+            "Women",
+            "Collection",
+            "Sale up to 50% Off",
+            "Accessories",
+            "Join Life",
+            "T-Shirts & Tops",
+            "Jeans",
+            "Best Sellers",
+            "Dresses"
+        ]
+    }
+    return (
+        // {isActive : (  ) }
+        <>
+            {isActive ? (
+                <ul className={'nav-menu active'}>
+                    {menuList.map((items , itemKey) => (
+                        <li key={itemKey}> {items}</li>
+                    ))}
+                </ul>
+            ) : null}
+        </>
+    )
+
+}
