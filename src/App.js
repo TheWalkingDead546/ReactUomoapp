099d6af38299fc8e3a00038aed6e6362c938d2c2
Ziyadk147 <ziyadkhan2333@gmail.com>
2024-09-14 09:32:29 -0700

    made rough top categories section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Navbar from './components/Navbar/Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Hero from "./components/Hero/Hero";
+import ProductContainer from "./components/ProductContainer/ProductContainer";
+import TopCategory from "./components/TopCategory/TopCategory";
 
 function App() {
   return (
     <>
         <Navbar />
-      <Hero />
+        <Hero />
+        <TopCategory />
     </>
   )
 }
